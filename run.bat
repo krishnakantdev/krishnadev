f323f847737239e46a039c56f94d42b57d7a6956
krishnakantdev <114336910+krishnakantdev@users.noreply.github.com>
2022-11-26 10:49:37 -0800

    Add files via upload

---
+++ run.bat
@@ -0,0 +1,6 @@
+@echo off
+
+python -m krishna sample_input\input1.txt
+python -m krishna sample_input\input2.txt
+python -m krishna sample_input\input3.txt
+python -m krishna sample_input\input4.txt
